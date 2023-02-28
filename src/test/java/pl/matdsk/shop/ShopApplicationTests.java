f86eda8c835cc05886e20a3f32dfc01879490020
matdsk <matdsk.work@gmail.com>
2023-02-28 20:23:24 +0100

    initial commit

---
+++ src/test/java/pl/matdsk/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matdsk.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ShopApplication.class)
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
